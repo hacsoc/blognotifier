d5070a00d0596bdacd59432425ebfc0c045653e4
Stephen Brennan <smb196@case.edu>
2016-03-15 13:06:08 -0400

    Initial commit with pretty much working bot.

---
+++ blognotifier.py
@@ -0,0 +1,51 @@
+"""Slack bot that notifies a channel when a new article is posted to a blog."""
+
+import time
+import json
+
+import feedparser
+import requests
+
+from private import WEBHOOK
+from config import FEEDS
+
+
+class BlogNotifier(object):
+    """Blog notifier POSTS to Slack webhook when a feed is updated."""
+
+    def __init__(self, feed_urls, hook_url):
+        self._urls = feed_urls
+        feeds = [feedparser.parse(f) for f in feed_urls]
+        self._latests = [f.entries[0].published_parsed for f in feeds]
+        self._hook_url = hook_url
+
+    def update(self):
+        """Run periodically to check for updates and POST them to Slack."""
+        for i, (url, latest) in enumerate(zip(self._urls, self._latests)):
+            feed = feedparser.parse(url)
+            new_latest = feed.entries[0].published_parsed
+            if new_latest > latest:
+                # Record the new latest publication date.
+                self._latests[i] = new_latest
+                # This is roughly Slack formatting.  Should be good enough.
+                message = '<!channel>: *{title}* @ {site}\n'\
+                          '<{link}>'.format(site=feed.feed.title,
+                                            **feed.entries[0])
+                # We can make our message more pretty by adding JSON keys.
+                json_obj = {'text': message, 'username': 'Blog Notifier',
+                            'icon_emoji': ':swift:'}
+                # We should say that we're using JSON.
+                headers = {'Content-type': 'application/json'}
+                requests.post(self._hook_url, data=json.dumps(json_obj),
+                              headers=headers)
+
+    def run_forever(self, delay):
+        """Does what it sounds like.... runs forever."""
+        while True:
+            time.sleep(delay)
+            self.update()
+
+
+if __name__ == '__main__':
+    notifier = BlogNotifier(FEEDS, WEBHOOK)
+    notifier.run_forever(15 * 60)
---
+++ config.py
@@ -0,0 +1,5 @@
+# This better not 404, or you'll crash my bot >:(
+FEEDS = [
+    'http://brennan.io/blog/rss.xml',
+    'http://fixedpoint.xyz/feed.xml',
+]
